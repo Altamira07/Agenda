02e7aab3f2165c096f6accf46cf4579866810cc9
Luis Altamira <luisaltamira07@gmail.com>
2019-03-17 21:16:31 -0600

    Conexion a base de datos y mapeo de entidades

---
+++ Agenda/Models/Contacto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    [Table("Contactos")]
+    public class Contacto
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public String Nombre { get; set; }
+        public ICollection<Telefono> Telefonos { get; set; }
+    }
+}
---
+++ Agenda/Models/Telefono.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    [Table("Telefono")]
+    public class Telefono
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(16)]
+        public String Numero { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string Nombre { get; set; }
+
+        public Contacto Contacto { get; set; }
+        public int ContactoId { get; set; }
+
+    }
+}
